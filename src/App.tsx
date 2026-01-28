05bc3f8285778f2b9ed7389134587d71a44e9009
CODExJATIN <jjatinparmar54@gmail.com>
2026-01-28 15:23:47 +0530

    Added form submit logic using a single formData object.

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,56 @@
-function App() {
+import {useState} from "react";
 
+function App() {
+    const [formData, setFormData] = useState({
+        objectives:'',
+        keyResults:''
+    });
+
+    function changeInput(e : React.ChangeEvent<HTMLInputElement> ) {
+        console.log(formData);
+        setFormData({...formData, [e.target.name]: e.target.value});
+    }
 
     return (
         <div className="h-screen flex items-center justify-center bg-gray-100">
             <form
-                className="w-[400px] border border-black  p-4 flex flex-col"
-                onSubmit={() => {
-                    console.log("Submitted!");
+                className="w-100 border border-black p-4 flex flex-col rounded-xl transition-transform duration-300 hover:scale-[1.02]"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    console.log("Submitted with values: ", formData);
                 }}
             >
-                <h2 className="text-lg font-bold mb-4 text-center">
+                <h2 className="text-lg font-bold mb-4 text-center
+                   animate-bounce">
                     OKR
                 </h2>
 
                 <input
                     type="text"
-                    className="border border-black p-2 mb-3"
+                    className="border border-black rounded-2xl p-2 mb-3 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-black"
                     placeholder="Objectives"
                     name="objectives"
+                    onChange={(e) => changeInput(e)}
                 />
 
                 <input
                     type="text"
-                    className="border border-black p-2 mb-4"
+                    className="border border-black rounded-2xl p-2 mb-4 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-black"
                     placeholder="Key Results"
                     name="keyResults"
+                    onChange={(e)=>changeInput(e)}
                 />
 
                 <button
                     type="submit"
-                    className="bg-blue-500 text-white p-2 mb-2"
+                    className="bg-black text-white p-2 mb-2 rounded-xl transition-all duration-300 hover:bg-gray-700 hover:scale-105 active:scale-95"
                 >
                     Submit
                 </button>
 
                 <button
                     type="reset"
-                    className="bg-red-400 text-white p-2"
+                    className="bg-red-400 text-white p-2 transition-all rounded-xl duration-300 hover:bg-red-500 hover:scale-105 active:scale-95"
                 >
                     Clear
                 </button>
